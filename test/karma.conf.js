a3a45d91ef84faa8c41e7c5fa42afd7990d2e536
eps1lon <silbermann.sebastian@gmail.com>
2021-03-16 11:48:43 +0100

    Same conf as core

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,4 +1,5 @@
 const playwright = require("playwright");
+
 process.env.CHROME_BIN = playwright.chromium.executablePath();
 
 module.exports = function setKarmaConfig(karmaConfig) {
@@ -8,18 +9,22 @@ module.exports = function setKarmaConfig(karmaConfig) {
     frameworks: ["mocha", "webpack"],
     files: [{ pattern: "test/karma.tests.js", watched: false }],
     plugins: ["karma-mocha", "karma-chrome-launcher", "karma-webpack"],
-    /**
-     * possible values:
-     * - config.LOG_DISABLE
-     * - config.LOG_ERROR
-     * - config.LOG_WARN
-     * - config.LOG_INFO
-     * - config.LOG_DEBUG
-     */
-    logLevel: karmaConfig.LOG_INFO,
-    port: 9876,
     preprocessors: {
       "test/karma.tests.js": ["webpack"],
+    },
+    webpack: {
+      // module: {
+      //   rules: [
+      //     {
+      //       test: /\.(js|ts|tsx)$/,
+      //       loader: 'babel-loader',
+      //       exclude: /node_modules/,
+      //       options: {
+      //         envName: 'test',
+      //       },
+      //     },
+      //   ],
+      // },
     },
     customLaunchers: {
       chromeHeadless: {
